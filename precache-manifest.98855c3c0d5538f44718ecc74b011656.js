c4f76a999a3844329ac2cd4f24862ac5be5d8bae
vmoeak <dangpengfei123@gmail.com>
2021-04-23 18:25:49 +0800

    deploy

---
+++ precache-manifest.98855c3c0d5538f44718ecc74b011656.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a168ac3295fe184380",
+    "url": "/money-website/css/app.cf64fe65.css"
+  },
+  {
+    "revision": "d6de744885d588c22192",
+    "url": "/money-website/css/chunk-vendors.990cc820.css"
+  },
+  {
+    "revision": "3350642824bd95d191cb1a1daece3a72",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "62a168ac3295fe184380",
+    "url": "/money-website/js/app.33019107.js"
+  },
+  {
+    "revision": "d6de744885d588c22192",
+    "url": "/money-website/js/chunk-vendors.6ed9a1fc.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
